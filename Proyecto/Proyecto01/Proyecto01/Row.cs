d2e0b351196328fd268238240df2a62afd0ae792
oswilehi <oswilehi@gmail.com>
2017-04-29 19:37:38 -0600

    Added all the things from the other file :V

---
+++ Proyecto/Proyecto01/Proyecto01/Row.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructuresURL_3._0;
+
+namespace Proyecto01
+{
+    class Row : IStringParseable<Row>
+    {
+
+        Dictionary<string, Int> intColumnsDictionary;
+        Dictionary<string, Varchar> varcharColumnsDictionary;
+        Dictionary<string, TimeDate> timeDateColumnsDictionary;
+
+        string IStringParseable<Row>.DEFAULT_FORMAT_
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        string IStringParseable<Row>.DEFAULT_MIN_VAL_FORMAT
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        int IStringParseable<Row>.objectLength
+        {
+            get
+            {
+                return intColumnsDictionary.Count + varcharColumnsDictionary.Count + timeDateColumnsDictionary.Count;
+            }
+        }
+
+        Row IStringParseable<Row>.ParseToObjectType(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IStringParseable<Row>.ParseToString(Row obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Proyecto/Proyecto01/Proyecto01/SQLFunctions.cs
+++ Proyecto/Proyecto01/Proyecto01/SQLFunctions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataStructuresURL_3._0;
 
 namespace Proyecto01
 {
@@ -41,7 +42,62 @@ namespace Proyecto01
         }
         public static void CreateTable(string createTableInstructions)
         {
-            
+
+            /*string createTableInstructions = "tableName||columnName,type,specialAttribute|columnName,type,specialAttribute";*/
+
+            // Separar tableName de Columnas con arreglo generalFields
+            string[] generalFields = createTableInstructions.Split("||".ToCharArray());
+            string tableName = generalFields[0];
+            //Separar columnas con arreglo columns
+            string[] columns = generalFields[1].Split('|');
+
+            int order = 5;
+            Row TableColumns = new Row();
+
+            //Leer columnas de arreglo columns
+            for (int i = 0; i < columns.Length; i++)
+            {
+                //Separar atributo de columnas con arreglo columnAttributes
+                string[] columnAttributes = columns[0].Split(',');
+
+                // Crear arbol b cuando encontremos la columna que tiene el specialAttribute == KEY
+                if (columnAttributes[2] == "KEY")
+                    // Crear arbol de acuerdo al tipo de KEY requerido
+                    switch (columnAttributes[1])
+                    {
+                        case "INT":
+                            BTree<Int, Row> intTableBTree = new BTree<Int, Row>(order, Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        case "VARCHAR-100":
+                            BTree<Varchar, Row> varcharTableBTree = new BTree<Varchar, Row>(order, Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        case "DATETIME":
+                            BTree<TimeDate, Row> timeDateTableBTree = new BTree<TimeDate, Row>(order, Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        default:
+                            break;
+                    }
+
+                // Columna no tiene specialAttribute == KEY
+                else
+                    //Agregar columna correspondiente a nuestra tabla
+                    switch (columnAttributes[1])
+                    {
+                        // EMPIEZO A CREER QUE ESTO NO ES DEL TODO NECESARIO...
+                        // NOTA: Necesitamos metodos para instanciar un arbol leyendolo desde un archivo...
+                        case "INT":
+                            BTree<Int, Row> intTableBTree = BTree<Int, Row>.ReadBTreeFromFile(Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        case "VARCHAR-100":
+                            //BTree<Varchar, TableColsValue> varcharTableBTree = new BTree<Varchar, TableColsValue>(order, Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        case "DATETIME":
+                            //BTree<TimeDate, TableColsValue> timeDateTableBTree = new BTree<TimeDate, TableColsValue>(order, Program.APPLICATION_TABLES_PATH + tableName);
+                            break;
+                        default:
+                            break;
+                    }
+            }
         }
 
         public static void Select(string selectInstructions)
